a16ceab86ad522431c88e23cc14f27993b9100dc
Richard Hawkins <Richard.Hawkins@revature.com>
2022-07-06 16:36:51 -0400

    Add CSharpNotes and examples

---
+++ wk1/CS/CSharpNotes.cs
@@ -0,0 +1,20 @@
+// Some C# Notes!
+
+// this is a comment
+
+Console.WriteLine(); //Writes to the console.
+Console.Write(); //Writes to the console, but it does not automatically end the line
+Console.ReadLine(); //Reads from the console.
+
+// EVERYTHING IS AN OBJECT
+
+// An object is ...
+// a real world entity with state and behaviour.
+
+// Data Types
+
+int x = 5; //integer - (whole number)        (1)0000000000000000000000
+float y = 5.5; //float
+double z = 5.5; //double
+long a = 5; //long
+short b = 5; //short
