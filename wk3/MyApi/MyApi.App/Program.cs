4df32f0fd65163c04b4161c8bfc3d46ec415882d
Richard Hawkins <Richard.Hawkins@revature.com>
2022-07-22 12:58:15 -0400

    add middleware

--- wk3/MyApi/MyApi.App/Program.cs
+++ wk3/MyApi/MyApi.App/Program.cs
@@ -1,25 +1,72 @@
+using System.Text.Json;
+using MyApi.Middleware;
+
 var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
-
+// before we "build" the WebApplication, we "set up" things that the middleware will need when they run
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-var app = builder.Build();
+WebApplication app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+// by default (customizable), for sercurity reasons, this built-in middleware 
+// only serve files that are inside a folder named wwwroot insinde the project
+app.UseStaticFiles();
 
-app.UseHttpsRedirection();
 
-app.UseAuthorization();
+// app.MapGet is part of "minimal api"
+app.MapGet("/", () => "hello world");
 
-app.MapControllers();
+
+// after you have a WebApplication, you need to construct it's request-processing pipeline using components called "middleware"
+// each middleware runs in sequence
+// "Use" middleware could be anywhere in the pipeline
+
+
+
+
+app.UseMiddleware<RequireAuthMiddleware>();
+// all of this below is replaced by this ^^
+//app.Use(async (context, next) =>
+//{
+//    if (context.Request.Query["authenticated"] == "true")
+//    {
+//        // this middleware is done, let the next process take over and do it's thing"
+//        await next(context);
+//    }
+//    else
+//    {
+//        // don't trigger the next, this middleware will "short-circuit" the pipeline
+//        // so we need to set up the response here.
+
+//        context.Response.StatusCode = 401;
+//        context.Response.ContentType = "text/plain";
+//        await context.Response.WriteAsync("error, not authenticated");
+//    }
+//});
+
+
+app.UseMiddleware<SerializeDataMiddleware>();
+//app.Use(async (context, next) =>
+//{
+//    if (context.Response.ContentType == "text/plain")
+//    {
+//        context.Response.ContentType = "application/json";
+//        context.Response.WriteAsync(JsonSerializer.Serialize(context.Response.Body));
+//    }
+//    else
+//    {
+//        await next(context);
+//    }
+//});
+
+
+
 
 app.Run();
