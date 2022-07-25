2b72f8e260bbf3ce4c21399948bc4bf9e1abeb57
Richard Hawkins <Richard.Hawkins@revature.com>
2022-07-25 16:16:46 -0400

    Add wk4 and TrainingAPI example

---
+++ wk4/TrainingAPI.App/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Text.Json;
+
+namespace TrainingAPI.App
+{
+    class Program
+    {
+        // Fields
+        public static readonly HttpClient client = new HttpClient();
+        public static string uri = "https://localhost:7059/api/Associates";
+        
+        
+        // Methods
+        static async Task Main()
+        {
+            string response = await client.GetStringAsync(uri);
+
+
+            List<AssociateDTO> associates = JsonSerializer.Deserialize<List<AssociateDTO>>(response);
+
+            foreach (var item in associates)
+            {
+                Console.WriteLine(item.ToString());
+            }
+        }
+    }
+}
---
+++ wk4/TrainingAPI/TrainingAPI.API/Program.cs
@@ -0,0 +1,32 @@
+
+
+using TrainingAPI.Data;
+
+string connectionString = await File.ReadAllTextAsync("C:/Revature/ConnectionStrings/220705-DB.txt");
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddSingleton<IRepository>(sp => new SQLRepository(connectionString, sp.GetRequiredService<ILogger<SQLRepository>>()));
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
