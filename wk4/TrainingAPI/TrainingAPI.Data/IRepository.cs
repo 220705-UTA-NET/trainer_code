c226b6f7bb64f95bdb163c10f79dfe04ad4c5440
Richard Hawkins <Richard.Hawkins@revature.com>
2022-07-26 12:49:21 -0400

    SDLC Whiteboards

--- wk4/TrainingAPI/TrainingAPI.Data/IRepository.cs
+++ wk4/TrainingAPI/TrainingAPI.Data/IRepository.cs
@@ -5,5 +5,9 @@ namespace TrainingAPI.Data
     public interface IRepository
     {
         Task<IEnumerable<Associate>> GetAllAssociatesAsync();
+
+        Task<IEnumerable<Trainer>> GetAllTrainersAsync();
+        Task DeleteAssociatesAsync();
+        Task DeleteTrainerAsync();
     }
 }
